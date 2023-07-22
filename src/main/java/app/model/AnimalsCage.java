ed4e89c28586f8948490e9684c94139354a09c1e
HP OMEN <nord2002d@yandex.ru>
2023-07-23 02:32:16 +0300

    deleted extend Timer class

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AnimalsCage extends Timer {
+public class AnimalsCage {
 
     @Autowired
     private Animal dog;
