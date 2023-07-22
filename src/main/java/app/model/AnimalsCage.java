96dd05e44dad8a14740901fcb77f401058d963e5
HP OMEN <nord2002d@yandex.ru>
2023-07-23 02:28:25 +0300

    completed task

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AnimalsCage {
+public class AnimalsCage extends Timer {
 
     @Autowired
-    private Animal animal;
+    private Animal dog;
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(dog.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
